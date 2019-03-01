6a043ec96398c607e1147a46641b7734ddda5ef6
luk3Sky <ganindudananja@gmail.com>
2019-03-02 01:18:53 +0530

    Package updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import Dashboard from 'coreui-react-starter/React_Full_Project/src/containers/Full';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h2>My App</h2>
-        </header>
+        {/* <Dashboard /> */}
+        Hey!
       </div>
     );
   }
