306b02bdb1183295bacc3c7b47b04b053e1f15d7
luk3Sky <ganindudananja@gmail.com>
2019-04-07 11:59:52 +0530

    Update GraphFunctions.js

--- src/containers/Dashboard/Graph/GraphFunctions.js
+++ src/containers/Dashboard/Graph/GraphFunctions.js
@@ -1,7 +1,9 @@
 export function getFormattedTime(epoch) {
   var myDate = new Date(epoch * 1000);
   var date = myDate.toString().split(" ");
-  var year = myDate.toString().split(" ")[3].substring(2, 4);
-  const formattedDate = `${myDate.getDate()}-${date[1]}-${year}`;
+  var year = date[3];
+  var time = date[4];
+  
+  const formattedDate = `${myDate.getDate()}-${date[1]}-${year} ${time}`;
   return formattedDate;
 }
