df916c9493583812bc2e285d8477b1a15914ee3f
luk3Sky <ganindudananja@gmail.com>
2019-04-07 10:55:42 +0530

    Update Dashboard.js

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -3,15 +3,24 @@ import Form from "./Form/Form";
 
 class Dashboard extends Component {
   state = {
-    asset_index: null
+    asset_index: null,
+    symobl_names: null
   };
 
   componentDidMount() {
     var ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
+    var asset_index = null;
+    var active_symbols = null;
     ws.onopen = evt => {
       ws.send(
         JSON.stringify({
           asset_index: 1
+        })
+      );
+      ws.send(
+        JSON.stringify({
+          active_symbols: "brief",
+          product_type: "basic"
         })
       );
     };
@@ -19,9 +28,16 @@ class Dashboard extends Component {
     ws.onmessage = msg => {
       var data = JSON.parse(msg.data);
       console.log(data);
+      if (data.asset_index) {
+        asset_index = data.asset_index;
+      }
+      if (data.active_symbols) {
+        active_symbols = data.active_symbols;
+      }
       this.setState(
         {
-          asset_index: data.asset_index
+          asset_index: asset_index,
+          active_symbols: active_symbols
         },
         () => {
           console.log(this.state);
@@ -34,16 +50,16 @@ class Dashboard extends Component {
     return (
       <div className="container">
         <div className="jumbotron">
-          <h1 className="display-3">Hello, world!</h1>
+          <h1 className="display-3">Welcome!</h1>
           <p className="lead">
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
+            Welcome to the Automated Trading Platform where you can trade Binary
+            Options based on our predictions.
           </p>
           <hr className="my-4" />
           <div className="card">
             <div className="card-body" />
-            {this.state.asset_index ? (
-              <Form asset_index={this.state.asset_index} />
+            {this.state.asset_index && this.state.active_symbols ? (
+              <Form data={this.state} />
             ) : null}
           </div>
           <p className="lead">
