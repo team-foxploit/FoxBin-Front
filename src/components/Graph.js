72602aba31d5c169ea1f1a1c3f96bfceb5968c0a
luk3Sky <ganindudananja@gmail.com>
2019-04-07 10:55:30 +0530

    Update Graph.js

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,58 +1,126 @@
-import React, { Component } from "react";
-import { Chart } from "react-google-charts";
+import React, { PureComponent } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  ReferenceLine,
+  YAxis,
+  Tooltip,
+  Legend
+} from "recharts";
 
-class Graph extends Component {
+const data = [
+  {
+    name: "Page A",
+    uv: 4000,
+    pv: 2400,
+    amt: 2400
+  },
+  {
+    name: "Page B",
+    uv: 3000,
+    pv: 1398,
+    amt: 2210
+  },
+  {
+    name: "Page C",
+    uv: 2000,
+    pv: 9800,
+    amt: 2290
+  },
+  {
+    name: "Page D",
+    uv: 2780,
+    pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    uv: 1890,
+    pv: 4800,
+    amt: 2181
+  },
+  {
+    name: "Page F",
+    uv: 2390,
+    pv: 3800,
+    amt: 2500
+  },
+  {
+    name: "Page G",
+    uv: 3490,
+    pv: 4300,
+    amt: 2100
+  }
+];
+
+class CustomizedLabel extends PureComponent {
   render() {
-    // var ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
+    const { x, y, stroke, value } = this.props;
 
-    // ws.onopen = function(evt) {
-    //   ws.send(JSON.stringify({ ticks: "frxAUDJPY" }));
-    // };
-
-    // ws.onmessage = function(msg) {
-    //   var data = JSON.parse(msg.data);
-    //   console.log(data);
-    // };
-
-    const data = [
-      [
-        "Day",
-        "Guardians of the Galaxy",
-        "The Avengers",
-        "Transformers: Age of Extinction"
-      ],
-      [1, 37.8, 80.8, 41.8],
-      [2, 30.9, 69.5, 32.4],
-      [3, 25.4, 57, 25.7],
-      [4, 11.7, 18.8, 10.5],
-      [5, 11.9, 17.6, 10.4],
-      [6, 8.8, 13.6, 7.7],
-      [7, 7.6, 12.3, 9.6],
-      [8, 12.3, 29.2, 10.6],
-      [9, 16.9, 42.9, 14.8],
-      [10, 12.8, 30.9, 11.6],
-      [11, 5.3, 7.9, 4.7],
-      [12, 6.6, 8.4, 5.2],
-      [13, 4.8, 6.3, 3.6],
-      [14, 4.2, 6.2, 3.4]
-    ];
     return (
-      <Chart
-        width={"600px"}
-        height={"400px"}
-        chartType="Line"
-        loader={<div>Loading Chart</div>}
-        data={data}
-        options={{
-          chart: {
-            title: "Box Office Earnings in First Two Weeks of Opening",
-            subtitle: "in millions of dollars (USD)"
-          }
-        }}
-        rootProps={{ "data-testid": "3" }}
-      />
+      <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">
+        {value}
+      </text>
     );
   }
 }
 
-export default Graph;
+class CustomizedAxisTick extends PureComponent {
+  render() {
+    const { x, y, payload } = this.props;
+
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text
+          x={0}
+          y={0}
+          dy={16}
+          textAnchor="end"
+          fill="#666"
+          transform="rotate(-35)"
+        >
+          {payload.value}
+        </text>
+      </g>
+    );
+  }
+}
+
+export default class Example extends PureComponent {
+  static jsfiddleUrl = "https://jsfiddle.net/alidingling/5br7g9d6/";
+
+  render() {
+    return (
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 10
+        }}
+      >
+        <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
+        <YAxis />
+        <Tooltip cursor={{ stroke: "red", strokeDasharray: "3 3" }} />
+        <Legend />
+        {/* <ReferenceLine x="Page C" stroke="red" strokeDasharray="3 3" />
+        <ReferenceLine
+          y={9800}
+          label="Max"
+          stroke="red"
+          strokeDasharray="3 3"
+        /> */}
+        <Line
+          type="monotone"
+          dataKey="pv"
+          stroke="#8884d8"
+          label={<CustomizedLabel />}
+        />
+      </LineChart>
+    );
+  }
+}
