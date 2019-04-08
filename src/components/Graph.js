b25f911817ac6dc52e36e675e29faa30dfff64e6
luk3Sky <ganindudananja@gmail.com>
2019-04-09 00:23:28 +0530

    Update Graph.js

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -4,10 +4,13 @@ import {
   Line,
   XAxis,
   ReferenceLine,
+  ResponsiveContainer,
   YAxis,
   Tooltip,
   Legend
 } from "recharts";
+import { getFormattedTime } from '../containers/Dashboard/Graph/GraphFunctions';
+import axios from 'axios';
 
 const data = [
   {
@@ -57,7 +60,6 @@ const data = [
 class CustomizedLabel extends PureComponent {
   render() {
     const { x, y, stroke, value } = this.props;
-
     return (
       <text x={x} y={y} dy={-4} fill={stroke} fontSize={10} textAnchor="middle">
         {value}
@@ -69,7 +71,7 @@ class CustomizedLabel extends PureComponent {
 class CustomizedAxisTick extends PureComponent {
   render() {
     const { x, y, payload } = this.props;
-
+    console.log(this.props);
     return (
       <g transform={`translate(${x},${y})`}>
         <text
@@ -87,23 +89,110 @@ class CustomizedAxisTick extends PureComponent {
   }
 }
 
-export default class Example extends PureComponent {
+export default class Graph extends PureComponent {
   static jsfiddleUrl = "https://jsfiddle.net/alidingling/5br7g9d6/";
+  
+  state = {
+    data: [],
+    count: 5
+  }
+
+  componentDidMount(){
+    
+    const subscribe = {
+        ticks_history: 'R_50',
+        end: 'latest',
+        start: 1,
+        style: 'ticks',
+        subscribe: 1,
+        adjust_start_time: 1,
+        count: this.state.count
+    };
+
+    this.ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
+
+    this.ws.onopen = () => {
+      this.ws.send(JSON.stringify(subscribe));
+    };
+
+    this.ws.onmessage = e => {
+      const value = JSON.parse(e.data);
+      console.log(value);
+      
+      var prevData = this.state.data;
+      if (value.msg_type === "history") {
+        console.log("his");
+        console.log(value.history.prices, value.history.times);
+        for (let i = 0; i < this.state.count; i++) {
+          const tempData = {
+            time: getFormattedTime(value.history.times[i]),
+            price: value.history.prices[i]
+          }
+          prevData.push(tempData);
+        }
+        this.setState({
+          data: prevData
+        },() => {
+          console.log(this.state.data);
+        });
+      }else if(value.msg_type === 'tick'){
+        console.log("now");
+        const tempData = {
+          time: getFormattedTime(value.tick.epoch),
+          price: value.tick.quote
+        }
+        // var prevData = {...this.state.data};
+        prevData.push(tempData);
+        this.setState({
+          data: prevData
+        }, () => {
+          console.log(this.state.data);           
+        });
+        // console.log(getFormattedTime(value.tick.epoch), value.tick.quote);        
+      }
+      
+
+      /*
+      axios.get('http://localhost:8000/api/tickhistory/')
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          data: res.data
+        });      
+      })
+      .catch(err => {
+        console.log(err);        
+      })
+      */
+      // const oldBtcDataSet = this.state.data;
+      // const newBtcDataSet = { ...oldBtcDataSet };
+      // newBtcDataSet.data.push(value.price);
+
+      // const newChartData = {
+      //   ...this.state.data,
+      //   datasets: [newBtcDataSet],
+      //   labels: this.state.lineChartData.labels.concat(
+      //     new Date().toLocaleTimeString()
+      //   )
+      // };
+      // this.setState({ lineChartData: newChartData });
+    };
+  }
 
   render() {
     return (
       <LineChart
-        width={500}
-        height={300}
-        data={data}
+        width={1000}
+        height={800}
+        data={this.state.data}
         margin={{
           top: 20,
-          right: 30,
+          right: 20,
           left: 20,
-          bottom: 10
+          bottom: 20
         }}
       >
-        <XAxis dataKey="name" height={60} tick={<CustomizedAxisTick />} />
+        <XAxis dataKey="time" height={60} tick={<CustomizedAxisTick />} />
         <YAxis />
         <Tooltip cursor={{ stroke: "red", strokeDasharray: "3 3" }} />
         <Legend />
@@ -116,7 +205,7 @@ export default class Example extends PureComponent {
         /> */}
         <Line
           type="monotone"
-          dataKey="pv"
+          dataKey="price"
           stroke="#8884d8"
           label={<CustomizedLabel />}
         />
