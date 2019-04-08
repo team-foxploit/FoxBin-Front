0ff87c3d381bf16c442f9bb17f39c8016c21e439
luk3Sky <ganindudananja@gmail.com>
2019-04-09 00:14:13 +0530

    Real time graph updated

--- src/containers/Dashboard/Plot/Plot.js
+++ src/containers/Dashboard/Plot/Plot.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Plot from "react-plotly.js";
-import { getFormattedTime } from '../Graph/GraphFunctions';
+import { getFormattedTime } from "../Graph/GraphFunctions";
 
 class PlotApp extends Component {
   constructor(props) {
@@ -8,43 +8,36 @@ class PlotApp extends Component {
     this.state = {
       data: [
         {
-          x: [
-            '06-Apr-2019 22:46:42',
-            '06-Apr-2019 22:46:44',
-            '06-Apr-2019 22:46:46'
-          ],
-          y: [
-            273.4472,
-            273.4704,
-            273.4816
-          ],
+          x: [],
+          y: [],
           type: "line",
           mode: "lines+markers",
           marker: {
-              color: "red"
-            }
+            color: "#ab63fa"
+          }
         }
       ],
       layout: {
-        width: 620,
-        height: 540,
-        title: "Trading values for USD/AUD"
+        width: 800,
+        height: 600,
+        title: "Trading values for USD/AUD",
+        datarevision: 0
       },
       frames: [],
-      config: {}
+      config: {},
+      revision: 0
     };
   }
 
-  componentDidMount(){
-    
+  componentDidMount() {
     const subscribe = {
-        ticks_history: 'R_50',
-        end: 'latest',
-        start: 1,
-        style: 'ticks',
-        subscribe: 1,
-        adjust_start_time: 1,
-        count: 5
+      ticks_history: "R_50",
+      end: "latest",
+      start: 1,
+      style: "ticks",
+      subscribe: 1,
+      adjust_start_time: 1,
+      count: 5
     };
 
     this.ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
@@ -55,49 +48,57 @@ class PlotApp extends Component {
 
     this.ws.onmessage = e => {
       const value = JSON.parse(e.data);
-      console.log(value);
-      
       var prevData = this.state.data[0];
+      var prevLayout = this.state.layout;
       if (value.msg_type === "history") {
         console.log("his");
         console.log(value.history.prices, value.history.times);
         for (let i = 0; i < this.state.count; i++) {
-        //   const tempData = {
-        //     time: getFormattedTime(value.history.times[i]),
-        //     price: value.history.prices[i]
-        //   }
           prevData.x.push(getFormattedTime(value.history.times[i]));
           prevData.y.push(value.history.prices[i]);
         }
-        console.log(prevData);        
-        // this.setState({
-        //   data: prevData
-        // },() => {
-        //   console.log(this.state.data);
-        // });
-      }else if(value.msg_type === 'tick'){
+        this.setState(
+          {
+            data: [prevData],
+            revision: this.state.revision + 1,
+            layout: {
+              ...this.state.layout,
+              datarevision: prevLayout.datarevision + 1
+            }
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      } else if (value.msg_type === "tick") {
         console.log("now");
-        // const tempData = {
-        //   time: getFormattedTime(value.tick.epoch),
-        //   price: value.tick.quote
-        // }
-        // var prevData = {...this.state.data};
         prevData.x.push(getFormattedTime(value.tick.epoch));
         prevData.y.push(value.tick.quote);
-        // this.setState({
-        //   data: prevData
-        // }, () => {
-        //   console.log(this.state.data);           
-        // });
-        console.log(prevData);        
-        // console.log(getFormattedTime(value.tick.epoch), value.tick.quote);        
+        this.setState(
+          {
+            data: [prevData],
+            revision: this.state.revision + 1,
+            layout: {
+              ...this.state.layout,
+              datarevision: prevLayout.datarevision + 1
+            }
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
       }
-      
     };
   }
 
   render() {
-    return <Plot data={this.state.data} layout={this.state.layout} />;
+    return (
+      <Plot
+        data={this.state.data}
+        layout={this.state.layout}
+        revision={this.state.revision}
+      />
+    );
   }
 }
 
