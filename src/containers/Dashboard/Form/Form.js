a4a2f9a264252846b76d56631b882e3c02358046
luk3Sky <ganindudananja@gmail.com>
2019-04-10 00:55:35 +0530

    Form updadet

--- src/containers/Dashboard/Form/Form.js
+++ src/containers/Dashboard/Form/Form.js
@@ -1,4 +1,5 @@
 import React from "react";
+import style from './Form.module.css';
 
 const Form = props => {
   // var ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
@@ -18,17 +19,18 @@ const Form = props => {
   //   console.log(props.data);
   // }
   return (
-    <div>
+    <div className={style.Left}>
+    <h3 className="login-heading mb-4">Welcome back!</h3>
       <form>
-        <div className="form-row">
+        {/* <div className="form-row">
           <div className="form-group col">
             <select
               id="inputState"
               className="form-control"
               defaultValue={props.data.asset_index[0][0]}
-            >
+            > */}
               {/* <option selected>{props.data.asset_index[0][1]}</option> */}
-              {props.data.asset_index.map(index => (
+              {/* {props.data.asset_index.map(index => (
                 <option key={index[1]}>{index[1]}</option>
               ))}
             </select>
@@ -78,7 +80,21 @@ const Form = props => {
               </div>
             </div>
           </div>
+        </div> */}
+        <div className="col">
+          <h5 className="">Login to trade on Binary.com platform</h5>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email address" />
+          {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
         </div>
+        <div className="col">
+          {/* <label htmlFor="exampleInputPassword1">Password</label> */}
+          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Remember me</label>
+        </div>
+        <button type="submit" className="btn btn-primary">SIGN IN</button>
       </form>
     </div>
   );
