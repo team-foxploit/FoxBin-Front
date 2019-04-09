8203cb028e8f84af43510eaf148d2c2413cc6aae
luk3Sky <ganindudananja@gmail.com>
2019-04-09 13:20:31 +0530

    Divided Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Dashboard from "./containers/Dashboard/Dashboard";
+import Dashboard from "./containers/Dashboard/Dashboard";
 // import Graph from "./components/Graph";
+import Navbar from './components/Navbar/Navbar';
 import Plot from './containers/Dashboard/Plot/Plot';
 
 class App extends Component {
@@ -9,52 +10,12 @@ class App extends Component {
     return (
       <div className="App">
         {/* <Dashboard /> */}
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <a className="navbar-brand" href="/">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarColor01"
-            aria-controls="navbarColor01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item active">
-                <a className="nav-link" href="/">
-                  Home <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Features
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  Pricing
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <Navbar />
         {/* <div className="center">
-          <Dashboard />
         </div> */}
+        <Dashboard />
         {/* <Graph /> */}
-        <Plot />
+        <Plot asset="EUR/USD" />
       </div>
     );
   }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="/">
+            FoxBinary
+        </a>
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarColor01"
+            aria-controls="navbarColor01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">
+                        Home <span className="sr-only">(current)</span>
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        Features
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        More
+                    </a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">
+                        About
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar;
