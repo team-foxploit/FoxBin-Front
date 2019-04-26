0b520dbc03d6ed03aa702d06ef5553250b1527c3
luk3Sky <ganindudananja@gmail.com>
2019-04-27 01:28:54 +0530

    Components arranged

--- src/containers/Dashboard/Auth/Register.js
+++ src/containers/Dashboard/Auth/Register.js
@@ -2,6 +2,115 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 class Register extends Component {
+  state = {
+    user: {
+      username: {
+        value: null,
+        valid: false
+      },
+      email: {
+        value: null,
+        valid: false
+      },
+      first_name: {
+        value: null,
+        valid: false
+      },
+      last_name: {
+        value: null,
+        valid: false
+      },
+      password: {
+        value: null,
+        valid: false
+      },
+      password2: {
+        value: null,
+        valid: false
+      }
+    },
+    formValidity: false,
+    password1Error: true,
+    password2Error: true
+  };
+
+  // triggered on change of states
+  inputChangedHandler = event => {
+    const updatedUser = {
+      ...this.state.user
+    };
+    const updatedFormElement = {
+      ...updatedUser[event.target.id]
+    };
+    updatedFormElement.value = event.target.value;
+    updatedFormElement.valid = this.validateField(
+      event.target.id,
+      updatedFormElement.value
+    );
+    updatedUser[event.target.id] = updatedFormElement;
+    this.setState({
+      user: updatedUser
+    });
+  };
+
+  validateField = (id, value) => {
+    switch (id) {
+      case "email":
+        const emailTest = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        return emailTest.test(value);
+      case "password":
+        if (value.length >= 8) {
+          this.setState({
+            password1Error: false
+          });
+          return true;
+        } else {
+          this.setState({
+            password1Error: true
+          });
+        }
+        return false;
+      case "password2":
+        if (this.state.user.password.value === value) {
+          this.setState({
+            password2Error: false
+          });
+          return true;
+        } else {
+          this.setState({
+            password2Error: true
+          });
+        }
+        return false;
+      default:
+        return value.trim() !== "";
+    }
+  };
+
+  // triggered on Submit
+  handleSubmit = event => {
+    event.preventDefault();
+    let validity = true;
+    for (const key in this.state.user) {
+      validity = validity && this.state.user[key].valid;
+    }
+    this.setState(
+      {
+        formValidity: validity
+      }
+    );
+    if (this.state.formValidity) {
+      const user = {
+        username: this.state.user.username.value,
+        email: this.state.user.email.value,
+        first_name: this.state.user.first_name.value,
+        last_name: this.state.user.last_name.value,
+        password: this.state.user.password.value,
+      };
+      console.log(user);
+    }
+  };
+
   render() {
     return (
       <div style={{ marginTop: "12px" }}>
@@ -22,16 +131,17 @@ class Register extends Component {
               </small>
               <br />
               <br />
-              <form>
+              <form onSubmit={this.handleSubmit}>
                 <div className="form-row">
                   <div className="form-group col-md-6">
-                    <label htmlFor="registerFirstName">First Name</label>
+                    <label htmlFor="first_name">First Name</label>
                     <input
                       type="text"
                       className="form-control"
-                      id="registerFirstName"
+                      id="first_name"
                       aria-describedby="firstNameHelp"
                       placeholder="First Name"
+                      onChange={this.inputChangedHandler}
                     />
                     <small
                       id="firstNameHelp"
@@ -41,13 +151,14 @@ class Register extends Component {
                     </small>
                   </div>
                   <div className="form-group col-md-6">
-                    <label htmlFor="registerLastName">Last Name</label>
+                    <label htmlFor="last_name">Last Name</label>
                     <input
                       type="text"
                       className="form-control"
-                      id="registerLastName"
+                      id="last_name"
                       aria-describedby="lastNameHelp"
                       placeholder="Last Name"
+                      onChange={this.inputChangedHandler}
                     />
                     <small id="lastNameHelp" className="form-text text-primary">
                       *optional
@@ -55,26 +166,28 @@ class Register extends Component {
                   </div>
                 </div>
                 <div className="form-group">
-                  <label htmlFor="registerUsername">Username</label>
+                  <label htmlFor="username">Username</label>
                   <input
                     type="text"
                     className="form-control"
-                    id="registerUsername"
+                    id="username"
                     aria-describedby="userNameHelp"
                     placeholder="Username"
+                    onChange={this.inputChangedHandler}
                   />
                   <small id="userNameHelp" className="form-text text-muted">
                     Your username should be unique.
                   </small>
                 </div>
                 <div className="form-group">
-                  <label htmlFor="exampleInputEmail1">Email address</label>
+                  <label htmlFor="email">Email address</label>
                   <input
                     type="email"
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="email"
                     aria-describedby="emailHelp"
                     placeholder="Enter email"
+                    onChange={this.inputChangedHandler}
                   />
                   <small id="emailHelp" className="form-text text-muted">
                     We'll never share your email with anyone else.
@@ -82,36 +195,43 @@ class Register extends Component {
                 </div>
                 <div className="form-row">
                   <div className="form-group col-md-6">
-                    <label htmlFor="registerPassword">Password</label>
+                    <label htmlFor="password">Password</label>
                     <input
                       type="password"
                       className="form-control"
-                      id="registerPassword"
+                      id="password"
                       aria-describedby="passwordHelp"
                       placeholder="Password"
+                      onChange={this.inputChangedHandler}
                     />
-                    <small id="passwordHelp" className="form-text text-warning">
-                      Your password should be at least of 8 characters length.
-                    </small>
+                    {this.state.password1Error ? (
+                      <small
+                        id="passwordHelp"
+                        className="form-text text-warning"
+                      >
+                        Your password should be at least of 8 characters length.
+                      </small>
+                    ) : null}
                   </div>
 
                   <div className="form-group col-md-6">
-                    <label htmlFor="registerConfirmPassword">
-                      Confirm Password
-                    </label>
+                    <label htmlFor="password2">Confirm Password</label>
                     <input
                       type="password"
                       className="form-control"
-                      id="registerConfirmPassword"
+                      id="password2"
                       aria-describedby="confirmPasswordHelp"
                       placeholder="Re-type Password"
+                      onChange={this.inputChangedHandler}
                     />
-                    <small
-                      id="confirmPasswordHelp"
-                      className="form-text text-warning"
-                    >
-                      Passwords should be matched.
-                    </small>
+                    {this.state.password2Error ? (
+                      <small
+                        id="confirmPasswordHelp"
+                        className="form-text text-warning"
+                      >
+                        Passwords should be matched.
+                      </small>
+                    ) : null}
                   </div>
                 </div>
                 <button type="submit" className="btn btn-primary">
