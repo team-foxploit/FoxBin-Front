5a2bd2e382c8585038e6b479f527693ed13153f8
luk3Sky <ganindudananja@gmail.com>
2019-05-01 00:17:12 +0530

    Refresh proof added to dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -5,10 +5,9 @@ import propTypes from "prop-types";
 import { connect } from "react-redux";
 import { getBlogs } from "../../store/actions/blogActions";
 import { createMessage } from "../../store/actions/messageActions";
-// import { GET_BLOGS } from '../../store/actions/actionTypes';
-// import style from "./Dashboard.module.css";
+
 import Sidebar from "./Sidebar/BSidebar";
-import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
+// import Breadcrumb from "../../components/Breadcrumb/Breadcrumb";
 import Account from "./Account/Account";
 
 class Dashboard extends Component {
@@ -20,15 +19,14 @@ class Dashboard extends Component {
 
   static propTypes = {
     posts: propTypes.array.isRequired,
+    user: propTypes.object.isRequired,
     getBlogs: propTypes.func.isRequired
   };
 
   componentDidMount() {
     console.log(this.props);
-    // dispatch(createMessage("mounted!"));
     this.props.getBlogs();
     this.props.createMessage("LOL");
-
     /*
     var ws = new WebSocket("wss://ws.binaryws.com/websockets/v3?app_id=1089");
     var asset_index = null;
@@ -70,30 +68,6 @@ class Dashboard extends Component {
   }
 
   render() {
-    // {/* <div className="container-fluid">
-    //   <div className={style.JumbotronMargin}>
-    //     <div className="jumbotron">
-    //       <h1 className="display-3">FoxBinary</h1>
-    //       <p className="lead">
-    //         Welcome to the Automated Trading Platform where you can trade Binary
-    //         Options based on our predictions.
-    //       </p>
-    //       <hr className="my-4" />
-    //       <div className="row no-gutters justify-content-md-center">
-    //         <div className="col-md-4">
-    //           <img src={BusinessChatImg} alt="business-chart"/>
-    //         </div>
-    //         <div className="card">
-    //           <div className="card-body col-md-10" />
-    //             {this.state.asset_index && this.state.active_symbols ? (
-    //               <Form data={this.state} />
-    //             ) : null}
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div> */}
-    // {/* <div className="container-fluid"> */}
     return (
       <div className="wrapper">
         <Sidebar />
@@ -124,13 +98,14 @@ class Dashboard extends Component {
 
 const mapStateToProps = state => {
   return {
-    posts: state.blogReducer.posts
+    posts: state.blog.posts,
+    user: state.auth.user
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    getBlogs: getBlogs,
+    getBlogs:() => dispatch(getBlogs),
     createMessage: msg => dispatch(createMessage(msg))
   };
 };
