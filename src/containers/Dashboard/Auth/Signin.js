d9199ea8470f3792224efd9cd24ba04857d86240
luk3Sky <ganindudananja@gmail.com>
2019-05-01 18:24:02 +0530

    Signin page done

--- src/containers/Dashboard/Auth/Signin.js
+++ src/containers/Dashboard/Auth/Signin.js
@@ -44,81 +44,79 @@ class Signin extends Component {
     }
 
     return (
-      <div style={{ marginTop: "12px" }}>
-        <div
-          className="card col-lg-6 col-md-10 main-content"
-          style={{ padding: "0" }}
-        >
-          <div className="row no-gutters">
-            <div className="col-md-6">
-              <img
-                src={BusinessChatImg}
-                className="card-img"
-                alt="business_image"
-                style={{ height: "100%" }}
-              />
-            </div>
-            <div className="col-md-6">
-              <div className="card-body">
-                <h5 className="card-title">
-                  Login to trade on Binary.com platform
-                </h5>
-                <form onSubmit={this.onSubmitHandler}>
-                  <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="username"
-                      aria-describedby="emailHelp"
-                      placeholder="Username"
-                      onChange={this.inputChangeHandler}
-                      value={this.state.user.username}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      placeholder="Password"
-                      onChange={this.inputChangeHandler}
-                      value={this.state.user.password}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <div className="form-group form-check">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="rememberCheck"
-                      />
-                      <label
-                        className="form-check-label"
-                        htmlFor="rememberCheck"
-                      >
-                        Remember me
-                      </label>
+        <div className="container text-center col-lg-8 mb-3">
+            <div className="card bg-light">
+                <div className="row no-gutters">
+                    <div className="col-md-4">
+                        <img
+                            src={BusinessChatImg}
+                            className="card-img"
+                            alt="business_image"
+                            style={{ height: "100%" }}
+                            />
                     </div>
-                  </div>
-                  <div className="form-group">
-                    <button type="submit" className="btn btn-primary">
-                      SIGN IN
-                    </button>
-                  </div>
-                </form>
-                <div className="col">
-                  <small className="card-text">
-                    If you do not have an account, please{" "}
-                    <Link to="/register">register</Link> first.
-                  </small>
+                    <div className="col-md-8">
+                        <div class="card-header bg-transparent"><h5>Login to trade on Binary.com platform</h5></div>
+                        <div className="card-body">
+                            <h5 className="card-title">
+
+                            </h5>
+                            <form className="text-left" onSubmit={this.onSubmitHandler}>
+                                <div className="form-group">
+                                    <label htmlFor="username">Username</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="username"
+                                        aria-describedby="emailHelp"
+                                        placeholder="Username"
+                                        onChange={this.inputChangeHandler}
+                                        value={this.state.user.username}
+                                        />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input
+                                        type="password"
+                                        className="form-control"
+                                        id="password"
+                                        placeholder="Password"
+                                        onChange={this.inputChangeHandler}
+                                        value={this.state.user.password}
+                                        />
+                                </div>
+                                <div className="form-group">
+                                    <div className="form-group form-check">
+                                        <input
+                                            type="checkbox"
+                                            className="form-check-input"
+                                            id="rememberCheck"
+                                            />
+                                        <label
+                                            className="form-check-label"
+                                            htmlFor="rememberCheck"
+                                            >
+                                            Remember me
+                                        </label>
+                                    </div>
+                                </div>
+                                <div className="form-group text-center">
+                                    <button type="submit" className="btn btn-primary">
+                                        SIGN IN
+                                    </button>
+                                </div>
+                            </form>
+                            <div className="col">
+                                <small className="card-text">
+                                    If you do not have an account, please{" "}
+                                    <Link to="/register">register</Link> first.
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     );
   }
 }
