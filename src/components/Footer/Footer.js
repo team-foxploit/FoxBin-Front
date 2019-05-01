3f57211fd293762a0e37908dd4d79b81d8a376ef
luk3Sky <ganindudananja@gmail.com>
2019-05-02 01:27:48 +0530

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer className="footer bg-transparent mt-auto py-3">
+            <hr/>
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <p>This is an experimental project and please consider yourself not to don't take too far with it, and we'll not be liable if some bad happens...</p>
+                        <span className="text-center text-muted"><small>@foxploit</small></span>
+                    </div>
+                    <div className="col-md-6 text-center">
+                        <h5>Stay in touch with us for more!</h5>
+                        <ul className="list-inline">
+                            <li className="list-inline-item"><a href="https://www.facebook.com/FOXPLOIT-TEAM"><i class="material-icons">offline_bolt</i></a></li>
+                            <li className="list-inline-item"><a href="https://www.facebook.com/FOXPLOIT-TEAM"><i class="material-icons">offline_bolt</i></a></li>
+                            <li className="list-inline-item"><a href="https://www.facebook.com/FOXPLOIT-TEAM"><i class="material-icons">offline_bolt</i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
