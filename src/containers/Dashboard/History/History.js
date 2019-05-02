8bd62dfe48a33d5e5a56d745e23e409cb816657f
luk3Sky <ganindudananja@gmail.com>
2019-05-02 10:54:00 +0530

    Create History.js

---
+++ src/containers/Dashboard/History/History.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const History = (props) => {
+    return (
+        <div className="container text-center">
+            <h4 className="mb-4">This is based on both the platforms binary.com and foxbinary</h4>
+            <table className="table table-bordered table-hover">
+              <thead className="text-info">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Token</th>
+                  <th scope="col">Trade result</th>
+                  <th scope="col">Success rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>91%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>79%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td colSpan="2">Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>61%</td>
+                </tr>
+              </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default History
