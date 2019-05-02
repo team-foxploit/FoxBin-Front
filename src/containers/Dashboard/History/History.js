a128bc2435faa8811b68898641276d057e9696d2
luk3Sky <ganindudananja@gmail.com>
2019-05-03 01:25:05 +0530

    Update History.js

--- src/containers/Dashboard/History/History.js
+++ src/containers/Dashboard/History/History.js
@@ -37,6 +37,106 @@ const History = (props) => {
                 </tr>
               </tbody>
             </table>
+            <table className="table table-bordered table-hover">
+              <thead className="text-info">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Token</th>
+                  <th scope="col">Trade result</th>
+                  <th scope="col">Success rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>91%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>79%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td colSpan="2">Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>61%</td>
+                </tr>
+              </tbody>
+            </table>
+            <table className="table table-bordered table-hover">
+              <thead className="text-info">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Token</th>
+                  <th scope="col">Trade result</th>
+                  <th scope="col">Success rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>91%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>79%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td colSpan="2">Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>61%</td>
+                </tr>
+              </tbody>
+            </table>
+            <table className="table table-bordered table-hover">
+              <thead className="text-info">
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Token</th>
+                  <th scope="col">Trade result</th>
+                  <th scope="col">Success rate</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>91%</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Jacob</td>
+                  <td>Thornton</td>
+                  <td>@fat</td>
+                  <td>79%</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td colSpan="2">Larry the Bird</td>
+                  <td>@twitter</td>
+                  <td>61%</td>
+                </tr>
+              </tbody>
+            </table>
+
         </div>
     )
 }
