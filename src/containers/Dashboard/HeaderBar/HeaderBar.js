18c041c631695ec134356f7cf1bb6421ff17ccd1
luk3Sky <ganindudananja@gmail.com>
2019-05-04 03:00:19 +0530

    Logout link in the Headerbar styled

--- src/containers/Dashboard/HeaderBar/HeaderBar.js
+++ src/containers/Dashboard/HeaderBar/HeaderBar.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import propTypes from "prop-types";
 import './HeaderBar.css';
 
-import propTypes from "prop-types";
+import { logout } from "../../../store/actions/authActions";
 
 const HeaderBar = props => {
-    console.log(props.username);
   return (
         <div className="row bg-dark mb-0 d-flex justify-content-between">
             <nav className="nav">
-                <NavLink className="nav-link bg-info" href="#" onClick={props.constrolSidebar}><i className="material-icons sidebar-toggler">menu</i></NavLink>
-                <NavLink className="nav-link" href="/dashboard">Dashboard</NavLink>
+                <NavLink className="nav-link bg-info" to="/dashboard" onClick={props.constrolSidebar}><i className="material-icons sidebar-toggler">menu</i></NavLink>
+                <NavLink className="nav-link" to="/dashboard">Dashboard</NavLink>
             </nav>
             {props.username ?
-                <span class="navbar-text pr-3 pt-2 text-white-50">
-                    You're logged in as {props.username} (<span role="a" href="#">Logout</span>)
+                <span className="navbar-text pr-3 pt-2 text-white-50">
+                    You're logged in as {props.username} (<NavLink className="logout-link" role="button" onClick={props.logout}>Logout</NavLink>)
                 </span>
                     :
                 null
@@ -29,10 +29,11 @@ const mapStateToProps = state => {
     username: state.auth.user.username
   };
 };
-//
-// Navbar.propTypes = {
-//   isAuthenticated: propTypes.bool.isRequired,
-//   logout: propTypes.func.isRequired
-// };
 
-export default connect(mapStateToProps)(HeaderBar);
+HeaderBar.propTypes = {
+  username: propTypes.string.isRequired,
+  logout: propTypes.func.isRequired,
+  controlSidebar: propTypes.func.isRequired
+};
+
+export default connect(mapStateToProps, { logout })(HeaderBar);
