ab281f136f79bbd75896b628662e8015db1534fd
luk3Sky <ganindudananja@gmail.com>
2019-05-04 03:41:15 +0530

    Sidebar links paths changed

--- src/containers/Dashboard/Sidebar/IconSideBar.js
+++ src/containers/Dashboard/Sidebar/IconSideBar.js
@@ -9,27 +9,27 @@ const IconSideBar = props => {
         <div className="col-xm-1 px-0" id="sidebar">
             <ul className="nav flex-column">
                 <li classNames="nav-item shadow">
-                    <NavLink className="nav-link" to="/dashboard">
+                    <NavLink className="nav-link" exact to="/dashboard">
                         <i className="material-icons text-secondary">home</i>
                     </NavLink>
                 </li>
                 <li className="nav-item shadow">
-                    <NavLink className="nav-link" to="/history">
+                    <NavLink className="nav-link" to="/dashboard/history">
                         <i className="material-icons text-primary">history</i>
                     </NavLink>
                 </li>
                 <li className="nav-item shadow">
-                    <NavLink className="nav-link" to="/predictions">
+                    <NavLink className="nav-link" to="/dashboard/predictions">
                         <i className="material-icons text-success">swap_calls</i>
                     </NavLink>
                 </li>
                 <li className="nav-item shadow">
-                    <NavLink className="nav-link" to="/automata">
+                    <NavLink className="nav-link" to="/dashboard/automata">
                         <i className="material-icons text-info">border_vertical</i>
                     </NavLink>
                 </li>
                 <li className="nav-item shadow">
-                    <NavLink className="nav-link" to="/integra">
+                    <NavLink className="nav-link" to="/dashboard/integra">
                         <i className="material-icons text-danger">code</i>
                     </NavLink>
                 </li>
