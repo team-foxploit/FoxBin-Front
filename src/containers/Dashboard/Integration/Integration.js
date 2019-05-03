93adfb5ee6569b96aea2a296fb07ae86cc009c29
luk3Sky <ganindudananja@gmail.com>
2019-05-04 02:49:33 +0530

    Create Integration.js

---
+++ src/containers/Dashboard/Integration/Integration.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Integration extends Component {
+    render () {
+        return(
+            <h1>Hey</h1>
+        )
+    }
+}
+
+export default Integration;
