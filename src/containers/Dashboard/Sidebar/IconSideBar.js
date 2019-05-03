5a7bfdf9ee8eac4f46c6fe7a79063c34f8ea16ed
luk3Sky <ganindudananja@gmail.com>
2019-05-04 02:44:55 +0530

    ClassName error corrected

--- src/containers/Dashboard/Sidebar/IconSideBar.js
+++ src/containers/Dashboard/Sidebar/IconSideBar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import propTypes from "prop-types";
 import Fragment from "../../../hoc/Fragment";
 import { NavLink } from "react-router-dom";
 import "./IconSideBar.css";
@@ -8,28 +7,28 @@ const IconSideBar = props => {
   return (
     <Fragment>
         <div className="col-xm-1 px-0" id="sidebar">
-            <ul class="nav flex-column">
-                <li class="nav-item shadow">
+            <ul className="nav flex-column">
+                <li classNames="nav-item shadow">
                     <NavLink className="nav-link" to="/dashboard">
                         <i className="material-icons text-secondary">home</i>
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/history">
                         <i className="material-icons text-primary">history</i>
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/predictions">
                         <i className="material-icons text-success">swap_calls</i>
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/automata">
                         <i className="material-icons text-info">border_vertical</i>
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/integra">
                         <i className="material-icons text-danger">code</i>
                     </NavLink>
--- src/containers/Dashboard/Sidebar/Sidebar.js
+++ src/containers/Dashboard/Sidebar/Sidebar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import propTypes from "prop-types";
 import Fragment from "../../../hoc/Fragment";
 import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
@@ -8,32 +7,32 @@ const Sidebar = props => {
   return (
     <Fragment>
         <div className="col-md-3 d-none px-0 d-md-block sidebar">
-            <ul class="nav flex-column">
-                <li class="nav-item shadow">
+            <ul className="nav flex-column">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/dashboard">
                         <i className="material-icons text-secondary">home</i>
                         Home
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/history">
                         <i className="material-icons text-primary">history</i>
                         History
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/predictions">
                         <i className="material-icons text-success">swap_calls</i>
                         Predictions
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/automata">
                         <i className="material-icons text-info">border_vertical</i>
                         Automation
                     </NavLink>
                 </li>
-                <li class="nav-item shadow">
+                <li className="nav-item shadow">
                     <NavLink className="nav-link" to="/integra">
                         <i className="material-icons text-danger">code</i>
                         Integrations
