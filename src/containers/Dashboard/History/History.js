dc639e19b7f0ea3f8753b011ebbf423b869fb5bb
luk3Sky <ganindudananja@gmail.com>
2019-06-08 23:24:37 +0530

    PredictionHistory removed

--- src/containers/Dashboard/History/History.js
+++ src/containers/Dashboard/History/History.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import PredictionHistory from './PredictionHistory';
 import LoginHistory from './LoginHistory';
 
 const History = (props) => {
 
   return (
     <div className="col text-center">
-      <div className="row">
-        <div className="col">
-          <h4 className="mb-4">This is based on both the platforms binary.com and foxbinary</h4>
-          <div className="card text-left">
-            <div className="card-header">
-              <h3>Prediction History</h3>
-            </div>
-            <div className="card-body">
-              <PredictionHistory />
-            </div>
-          </div>
-        </div>
-      </div>
       <div className="row mt-4">
         <div className="col">
           <div className="card text-left">
